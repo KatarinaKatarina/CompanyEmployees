d8277aa80f65bda8ac6d28e9f4d480ec1003274e
Katarina Ivanic <katarinakacha90@yahoo.com>
2023-02-16 22:36:34 +0100

    Add CORS policy thought ServiceExtensions.cs

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions //is this in use now? Shouldn't it be called in Program.cs
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin() //WithOrigin("http://frontend-app.com")
+                .AllowAnyMethod() //WithMethods ("POST","GET") only this tpe of methods for example
+                .AllowAnyHeader()); //WithHeaders("content-type") etc.
+            });
+    }
+}
