dc626e68ba0193e892524913b6a298ed7ebc0f09
KatarinaIvanic <katarina.ivanic@htecgroup.com>
2023-09-07 08:52:57 +0200

    Implement GetEmployeeById ep; add EmployeeNotFoundException.cs

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -31,5 +31,21 @@ namespace Service
             var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
             return employeesDto;
         }
+
+        public EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges)
+        {
+            var company = _repository.Company.GetCompany(companyId, trackChanges);
+            
+            if (company is null)
+                throw new CompanyNotFoundException(companyId);
+            
+            var employeeDb = _repository.Employee.GetEmployee(companyId, id, trackChanges);
+            
+            if (employeeDb is null)
+                throw new EmployeeNotFoundException(id);
+            
+            var employee = _mapper.Map<EmployeeDto>(employeeDb);
+            return employee;
+        }
     }
 }
